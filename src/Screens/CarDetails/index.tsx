d6309f2963ef7e091c4ba40fc9b53a591e228c85
Ruan Pablo <ruan_goio10@hotmail.com>
2022-02-10 21:38:45 -0300

    Detalhes do cartão

--- src/Screens/CarDetails/index.tsx
+++ src/Screens/CarDetails/index.tsx
@@ -5,7 +5,16 @@ import { ImageSlider } from '../../Components/ImageSlider';
 
 import {
    Container,
-   Header
+   Header,
+   Content,
+   Details,
+   Description,
+   Brand,
+   Name,
+   Rent,
+   Period,
+   Price,
+   About,
 } from './styles';
 
 export function CarDetails(){
@@ -25,6 +34,28 @@ export function CarDetails(){
          <ImageSlider 
             imagesUrl={['https://images-ext-2.discordapp.net/external/bwumaL3tGp9p9XiCjbi9BgO_jgrd00BmeKLLv30OaKw/https/www.pngmart.com/files/4/Chevrolet-Camaro-PNG-Image.png']}
          />
+
+         <Content>
+            <Details>
+               <Description>
+                  <Brand>Lamborghini</Brand>
+                  <Name>Huracan</Name>
+               </Description>
+
+               <Rent>
+                  <Period>Ao dia</Period>
+                  <Price>R$ 580</Price>
+               </Rent>
+            </Details>
+
+            <About>
+               Este é automóvel desportivo. Surgiu do lendário touro de lide indultado 
+               na praça Real Maestranza de Sevilla. 
+               É um belíssimo carro para quem gosta de acelerar.
+            </About>
+
+            
+         </Content>
      </Container>
    );
 }
